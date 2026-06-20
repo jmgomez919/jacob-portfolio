6093edaa1e7c30744dd71e42cc8aeddae04ae29b
jmgomez919 <j.mgomez919@gmail.com>
2026-06-20 17:29:01 -0400

    Revise marketing impact description in Projects.js
    Updated the description of marketing strategies and their impact on viewership.

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -400,11 +400,12 @@ export default function Projects() {
                 Increased Median Viewership by <strong><u>84.4%</u></strong>
               </h3>
               <p className="viewership-stat__body">
-                As Marketing Director for Volunteer UCF, I led with a photo-first philosophy by favoring authentic
-                images of volunteers and events over graphic-heavy cover slides. By utilizing strategic profile tags
-                and UCF-specific hashtags, each post felt more personal and reached a wider audience organically.
+                As Marketing Director for Volunteer UCF, I led with a photo-first philosophy by favoring authentic, action-oriented
+                images of volunteers and events over graphic-heavy cover slides. I employed specific art styles that reflected each 
+                event and allowed each post to stand out from eachother. By utilizing strategic profile tags and UCF-specific hashtags,
+                  each post felt more personal and reached a wider audience organically.
                 Across my tenure from Fall 2025 through Spring 2026, median viewership nearly doubled as well as a 
-                  noticeable increase in audience engagement and overall exposure.
+                  noticeable increase in audience engagement and overall exposure, spreading the word of VUCF's mission.
               </p>
               <img
                 src="/images/vucf-viewership-graph.png"
