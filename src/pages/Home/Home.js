6cc91740f54838636c54bdd49912c279b3db9d8c
jmgomez919 <j.mgomez919@gmail.com>
2026-06-24 11:11:14 -0400

    Shift all hero slideshow images to 90% right position

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,14 +10,14 @@ import './Home.css';
 // position: CSS object-position  e.g. '75% center', 'right top', '50% 30%'
 // scale:    zoom multiplier       e.g. 1 = normal, 1.2 = 20% zoom in
 const slideshowImages = [
-  { src: '/images/home-slide-1.png', position: '90% 50%', scale: 1   },
-  { src: '/images/home-slide-2.png', position: '75% 50%', scale: 1   },
-  { src: '/images/home-slide-3.jpg', position: '75% 50%', scale: 1   },
-  { src: '/images/home-slide-4.jpg', position: '90% 50%', scale: 1   },
-  { src: '/images/home-slide-5.jpg', position: '75% center', scale: 1   },
-  { src: '/images/home-slide-6.jpg', position: '75% center', scale: 1   },
-  { src: '/images/home-slide-7.jpg', position: '75% center', scale: 1   },
-  { src: '/images/home-slide-8.jpg', position: '75% center', scale: 1   },
+  { src: '/images/home-slide-1.png', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-2.png', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-3.jpg', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-4.jpg', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-5.jpg', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-6.jpg', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-7.jpg', position: '90% center', scale: 1   },
+  { src: '/images/home-slide-8.jpg', position: '90% center', scale: 1   },
 ];
 
 function HeroSlideshow({ images }) {
