942847407898b473109dec7dddee8b20734e833c
jmgomez919 <j.mgomez919@gmail.com>
2026-06-24 13:27:53 -0400

    Remove unused slideInRight import to fix build

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import {
   pageVariants, fadeUp, staggerContainer,
-  slideInLeft, slideInRight, viewportOnce,
+  slideInLeft, viewportOnce,
 } from '../../utils/animations';
 import './Home.css';
 
